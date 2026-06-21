7d64fcb9b8c5f5c81ae061795983735f961f370d
vanrajboricha <vanraj723@gmail.com>
2026-06-21 12:34:07 +0530

    Revert changes back to stable June 18th state (d699515)

--- backstage/packages/app/src/App.tsx
+++ backstage/packages/app/src/App.tsx
@@ -13,7 +13,6 @@ import grafanaPlugin from '@backstage-community/plugin-grafana/alpha';
 import { EntityPrometheusContent } from '@roadiehq/backstage-plugin-prometheus';
 import argocdPlugin from '@roadiehq/backstage-plugin-argo-cd/alpha';
 import sonarQubePlugin from '@backstage-community/plugin-sonarqube/alpha';
-import kubernetesPlugin from '@backstage/plugin-kubernetes/alpha';
 
 const signInPage = SignInPageBlueprint.make({
   params: {
@@ -67,7 +66,6 @@ export default createApp({
     grafanaPlugin,
     argocdPlugin,
     sonarQubePlugin,
-    kubernetesPlugin,
     githubActionsPlugin,
     createFrontendModule({
       pluginId: 'app',
--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -57,14 +57,11 @@ backend.add(import('@backstage/plugin-search-backend-module-pg'));
 backend.add(import('@backstage/plugin-search-backend-module-catalog'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs'));
 
-// Cloud Providers
-backend.add(import('@backstage/plugin-catalog-backend-module-aws'));
-backend.add(import('@backstage/plugin-catalog-backend-module-azure'));
 //SOnarQube
 backend.add(import('@backstage-community/plugin-sonarqube-backend'));
 
 // kubernetes plugin
-//backend.add(import('@backstage/plugin-kubernetes-backend'));
+// backend.add(import('@backstage/plugin-kubernetes-backend'));
 
 // notifications and signals plugins
 backend.add(import('@backstage/plugin-notifications-backend'));
