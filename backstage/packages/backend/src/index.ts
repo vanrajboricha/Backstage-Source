fdb8d0399d9449a78694ecc4da020398c4f6f5f4
vanrajboricha <vanraj723@gmail.com>
2026-06-21 10:31:53 +0530

    Adding files

--- backstage/packages/backend/src/index.ts
+++ backstage/packages/backend/src/index.ts
@@ -64,7 +64,7 @@ backend.add(import('@backstage/plugin-catalog-backend-module-azure'));
 backend.add(import('@backstage-community/plugin-sonarqube-backend'));
 
 // kubernetes plugin
-backend.add(import('@backstage/plugin-kubernetes-backend'));
+//backend.add(import('@backstage/plugin-kubernetes-backend'));
 
 // notifications and signals plugins
 backend.add(import('@backstage/plugin-notifications-backend'));
